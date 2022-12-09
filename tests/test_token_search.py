da1267b5c02a305fa6996f943af53b6196c2624d
Doan Bui <bxdoan93@gmail.com>
2022-12-09 19:02:40 +0700

    fix test

--- tests/test_token_search.py
+++ tests/test_token_search.py
@@ -8,7 +8,7 @@ class TokenSearchTest(TestCase):
     def test_token_search(self):
         # Test token search by symbol
         list_tokens = TokenSearch(search_key='AAVE').search()
-        assert len(list_tokens) == 4
+        assert len(list_tokens) == 79
         val_EXPECT = {
             'symbol': 'AAVE', 'name': 'Aave', 'type': 'ERC20', 'address': '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
             'ens_address': '', 'decimals': 18, 'website': 'https://app.aave.com/?referral=93',
