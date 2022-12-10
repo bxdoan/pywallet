5fa2c1a62ea154b04eac5fb4ed6e2bd8822fbd43
Doan Bui <bxdoan93@gmail.com>
2022-12-11 01:16:15 +0700

    make it better

--- quicktest.sh
+++ quicktest.sh
@@ -14,6 +14,6 @@ function _() {
     fi
     mkdir -p "$SH/tmp"
     tee_log="$SH/tmp/$(basename $BASH_SOURCE).log"
-    PYTHONPATH=`pwd` $pipenv run pytest  -p no:warnings  --tb=short --lf   2>&1 | tee $tee_log
+    PYTHONPATH=`pwd` $pipenv run pytest  -p no:warnings  --tb=short 2>&1 | tee $tee_log
 }
   eval _
